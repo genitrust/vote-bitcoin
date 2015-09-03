6a4e1f151590fa3207d19edccf669bce85720a69
Robert Genito <rob@genitrust.com>
2015-09-02 22:21:05 -0400

    fixed fatal exceptions in management command

--- votebitcoin/management/commands/import_vote_cards.py
+++ votebitcoin/management/commands/import_vote_cards.py
@@ -1,25 +1,23 @@
+from optparse import make_option
+from bs4 import BeautifulSoup
+
 from django.core.management.base import BaseCommand, CommandError
 from votebitcoin.models import VotingCard
 
 class Command(BaseCommand):
     help = 'Imports voting cards from the Genitrust Note HTML document.'
 
-#    def add_arguments(self, parser):
-#        parser.add_argument('poll_id', nargs='+', type=int)
-    def add_arguments(self, parser):
-        parser.add_argument('--html', type=str)
-	
+    option_list = BaseCommand.option_list + (
+        make_option('--html', dest='html',
+                    help='Genitrust Notes Bulk HTML format.'),
+    )
 
     def handle(self, *args, **options):
-        import sys
-        from bs4 import BeautifulSoup
 #        soup = BeautifulSoup(open("notes_101-200.html"))
-        #self.stdout.write('you supplied the html option:', options['html'])
+        self.stdout.write('you supplied the html option: %s' % options['html'])
         soup = BeautifulSoup(open(options['html']),"html.parser")
-        
-#        sys.stdout = open('outputt2', 'w')
-        
-        alladdresses = soup.findAll('div',{'class':'btcaddress'})
+
+        alladdresses = soup.findAll('div', {'class':'btcaddress'})
         for address in alladdresses:
             v = VotingCard()
             v.publicKey = address
