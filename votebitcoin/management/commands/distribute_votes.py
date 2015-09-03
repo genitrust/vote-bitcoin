15d5aff66d1191f9744aed0805efb0dd1b7adaba
Robert Genito <rob@genitrust.com>
2015-09-02 22:50:35 -0400

    draft of distribute votes management command

--- votebitcoin/management/commands/distribute_votes.py
+++ votebitcoin/management/commands/distribute_votes.py
@@ -1,22 +1,74 @@
+from optparse import make_option
+from bs4 import BeautifulSoup
+
 from django.core.management.base import BaseCommand, CommandError
 from votebitcoin.models import VotingCard
 
 
 class Command(BaseCommand):
-    help = 'Outputs the signed transaction hex for broadcasting '
+    help = 'Outputs the signed transaction hex for broadcasting ' + \
         '(distributing) coins to all Vote Cards.'
 
-    def add_arguments(self, parser):
-        parser.add_argument('poll_id', nargs='+', type=int)
+    option_list = BaseCommand.option_list + (
+        make_option('--html', dest='html',
+            help='Genitrust Notes Bulk HTML format -- all files separated '
+            'by commas.'),
+        make_option('--input', dest='input',
+            help='Source of bitcoin as the input address.'),
+        make_option('--size', dest='size', type=int,
+            help='Distribution size in units (satoshi) per Vote Card.'),
+        make_option('--wif', dest='wif',
+            help='The input public key\'s corresponding private key for '
+            'signing the bitcoin transaction.'),
+        make_option('--change', dest='change',
+            help='Change address to send the remaining voting credits.'),
+    )
 
     def handle(self, *args, **options):
-        for poll_id in options['poll_id']:
-            try:
-                poll = Poll.objects.get(pk=poll_id)
-            except Poll.DoesNotExist:
-                raise CommandError('Poll "%s" does not exist' % poll_id)
+        self.stdout.write('html: %s' % options['html'].split(','))
+        self.stdout.write('input: %s' % options['input'])
+        self.stdout.write('size: %s' % options['size'])
+        self.stdout.write('wif: %s' % options['wif'])
+        self.stdout.write('change: %s' % options['change'])
 
-            poll.opened = False
-            poll.save()
+        htmlFiles = options['html'].split(',')
+        inputCoins = options['input']
+        unitSize = options['size']
+        signingWif = options['wif']
+        changeAddress = options['change']
 
-            self.stdout.write('Successfully closed poll "%s"' % poll_id)
+        # STEP 1: load all of the voting card bitcoin addresses from HTML files
+        btcAddresses = []
+        for htmlFile in htmlFiles:
+            # TODO: use a function here to scrape the stuff.
+            # this function should return a list of bitcoin addresses!!!
+            #btcAddresses += thatFunction(htmlFile)
+            pass
+
+        # STEP 2: build the transaction with all bitcoin addresses as the
+        # transaction outputs.
+        line = ['tx', '-i', inputCoins]
+
+        # add each voting card bitcoin address to the command 'line', followed
+        # by the amount of satoshi units.
+        for address in btcAddresses:
+            line += ['%s/%d' % (address, unitSize)]
+        line += [changeAddress]
+        # output of the unsigned transaction hex
+        unsignedTxFile = '%s_%s.unsigned.tx' % (inputCoins, changeAddress)
+        line += ['-o', unsignedTxFile]
+
+        # STEP 3: finally -- build the transaction!
+        # TODO:
+        # use Popen to run this baby? maybe a better solution since we do not
+        # need the text from the transaction creation...
+        self.stdout.write('Command for building the transaction: {}'.format(
+            ' '.join(line)
+        ))
+
+        # STEP 4: sign the transaction, with the output going directly to
+        # standard output.
+        signedTxFile = unsignedTxFile.replace('unsigned', 'signed')
+        line = ['tx', unsignedTxFile, '-f', signingWif]
+        # TODO: send the 'line' to the system command line, and allow the output
+        # to be displayed on the screen.
